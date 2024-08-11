3357b86abaf66b641530b1e69b4cad595f99a074
miracoly <68049792+miracoly@users.noreply.github.com>
2024-08-11 14:04:00 +0200

    feat: add initial vuepress

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,18 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'Apprentice Program',
+  description: 'Getting project-ready in three months.',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
